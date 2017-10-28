be27c9bc6cf78b30490efe2ce7b7c8d3d047b054
Ayman Achoua <achouaayman@gmail.com>
2017-10-28 10:08:25 +0100

    Assignment 3

--- Assign3/app.js
+++ Assign3/app.js
@@ -4,59 +4,90 @@
 	angular.module('NarrowItDownApp', [])
 	.controller('NarrowItDownController', NarrowItDownController)
 	.service('MenuSearchService',MenuSearchService)
+	.directive('foundItems', FoundItemsDirective);
+
+	function FoundItemsDirective() {
+	  	var ddo = {
+	     	templateUrl: 'menu-list.html',
+	     	scope: {
+			    found: '<',
+			    show: '<',
+			    wait: '<'
+		    },
+		    controller: FoundItemsDirectiveController,
+    		controllerAs: 'menu',
+    		bindToController: true
+	    };
+	    
+	  	return ddo;
+	}
+
+	function FoundItemsDirectiveController() {
+	  	var menu = this;
+
+	  	menu.removeFromMenu = function(index){
+			menu.found.splice(index, 1);
+		}
+
+	}
 
 	NarrowItDownController.$inject = ['$scope', 'MenuSearchService'];
 	function NarrowItDownController($scope, MenuSearchService) {
 		var menu = this;
+		
+		menu.showMessage = false;
+		menu.connectionInProgress = false;
 
 		menu.displaySearchMenu = function (str){
 			var str = $scope.menu_search;
-			MenuSearchService.getMatchedMenuItems(str).then(
-				function (foundItems) {
-		    	menu.foundItems = foundItems;
-		  		})
-		  	.catch(function (error) {
-		    console.log("Something went terribly wrong.");
-		  	});
+			menu.connectionInProgress = true;
 
+			if (str === undefined || str == ''){
+				menu.foundItems = [];
+				menu.showMessage = true;
+				menu.connectionInProgress = false;
+			}
+			else {
+				menu.showMessage = false;
+				MenuSearchService.getMatchedMenuItems(str)
+				.then(function (foundItems) {
+		    		menu.foundItems = foundItems;
+		    		menu.connectionInProgress = false;
+		    		if (menu.foundItems.length == 0) menu.showMessage = true;
+		  		})
+		  		.catch(function (error) {
+		    		console.log("Something went terribly wrong.");
+		  		});
+		  	}
 		}
 	};
 
 	MenuSearchService.$inject = ['$http','$q'];
 	function MenuSearchService($http,$q) {
 		var service = this;
+		var menu = [];
 
 		service.getMatchedMenuItems = function (searchTerm) {
 			var deferred = $q.defer();
-			var message = "";
-			var menu = [];
-			console.log(searchTerm);
 
 			var promise = $http({
 			method: "GET",
 			url: "https://davids-restaurant.herokuapp.com/menu_items.json"
 			});
-			console.log(promise);
-
+			
 			promise.then(function (response) {
-				console.log(searchTerm);
-				//return response.data.menu_items;
 		      	var foundItems = response.data.menu_items;
-		      	console.log(foundItems);
-		      	console.log(foundItems.length);
 		      	for (var i = 0; i < foundItems.length ; i++) {
-		      		//console.log(i + foundItems[i].name);
-		      		if (!foundItems[i].name.includes(searchTerm)){
-		      			console.log(foundItems[i].name);
+		      		if (!foundItems[i].description.toLowerCase().includes(searchTerm.toLowerCase()) && 
+		      			!foundItems[i].name.toLowerCase().includes(searchTerm.toLowerCase())) {
 		      			foundItems.splice(i,1);
 		      			i--;
 		      		}
 		      	}
-		      	console.log(foundItems);
 		      	deferred.resolve(foundItems);
 		    })
+
 		return deferred.promise;
 		}
 	};
-
 })();
